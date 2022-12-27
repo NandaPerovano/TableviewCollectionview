da83478a325d6794ebb3ce12be70a45c13e2ad43
NandaPerovano <nandaperovanorm@gmail.com>
2022-12-27 17:09:59 -0300

    Mudança na cor da tela

--- TableviewCollectionview/VC/PathologyVC.swift
+++ TableviewCollectionview/VC/PathologyVC.swift
@@ -11,6 +11,6 @@ class PathologyVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .orange
     }
 }
